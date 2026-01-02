c9d57458496bbb794c61e3cfc00786a19f8db380
Conny Brunnkvist <cbrunnkvist@gmail.com>
2026-01-02 23:29:48 +0700

    Remove legacy ES6 code and deprecated Travis CI
    - Delete src/ folder (replaced by lib/ TypeScript)
    - Delete .travis.yml (Travis CI no longer maintained)

--- src/index.js
+++
@@ -1,8 +0,0 @@
-const Money = require('./lib/money')
-const currencies = require('../currencymap.json')
-
-Object.keys(currencies).forEach((code) => {
-  Money.registerCurrency(code, currencies[code])
-})
-
-module.exports = Money
