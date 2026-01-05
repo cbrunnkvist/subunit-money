6f4274b8bbc5c5668714c15f08dd51f47cd24c13
Conny Brunnkvist <cbrunnkvist@gmail.com>
2026-01-05 15:38:03 +0700

    Build: migrate to tsup for robust dual CJS/ESM bundling

--- dist/index.cjs
+++ dist/index.cjs
@@ -1,11 +1,49 @@
+"use strict";
+var __defProp = Object.defineProperty;
+var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
+var __getOwnPropNames = Object.getOwnPropertyNames;
+var __hasOwnProp = Object.prototype.hasOwnProperty;
 var __typeError = (msg) => {
   throw TypeError(msg);
 };
+var __export = (target, all) => {
+  for (var name in all)
+    __defProp(target, name, { get: all[name], enumerable: true });
+};
+var __copyProps = (to, from, except, desc) => {
+  if (from && typeof from === "object" || typeof from === "function") {
+    for (let key of __getOwnPropNames(from))
+      if (!__hasOwnProp.call(to, key) && key !== except)
+        __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
+  }
+  return to;
+};
+var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: true }), mod);
 var __accessCheck = (obj, member, msg) => member.has(obj) || __typeError("Cannot " + msg);
 var __privateGet = (obj, member, getter) => (__accessCheck(obj, member, "read from private field"), getter ? getter.call(obj) : member.get(obj));
 var __privateAdd = (obj, member, value) => member.has(obj) ? __typeError("Cannot add the same private member more than once") : member instanceof WeakSet ? member.add(obj) : member.set(obj, value);
 var __privateSet = (obj, member, value, setter) => (__accessCheck(obj, member, "write to private field"), setter ? setter.call(obj, value) : member.set(obj, value), value);
 var __privateMethod = (obj, member, method) => (__accessCheck(obj, member, "access private method"), method);
+
+// lib/index.ts
+var index_exports = {};
+__export(index_exports, {
+  AmountError: () => AmountError,
+  CurrencyMismatchError: () => CurrencyMismatchError,
+  CurrencyUnknownError: () => CurrencyUnknownError,
+  ExchangeRateError: () => ExchangeRateError,
+  ExchangeRateService: () => ExchangeRateService,
+  Money: () => Money,
+  MoneyConverter: () => MoneyConverter,
+  SubunitError: () => SubunitError,
+  clearCurrencies: () => clearCurrencies,
+  getAllCurrencies: () => getAllCurrencies,
+  getCurrency: () => getCurrency,
+  hasCurrency: () => hasCurrency,
+  loadCurrencyMap: () => loadCurrencyMap,
+  registerCurrency: () => registerCurrency
+});
+module.exports = __toCommonJS(index_exports);
 
 // lib/errors.ts
 var CurrencyMismatchError = class _CurrencyMismatchError extends TypeError {
@@ -1249,7 +1287,8 @@ var currencymap_default = {
 
 // lib/index.ts
 loadCurrencyMap(currencymap_default);
-export {
+// Annotate the CommonJS export names for ESM import in node:
+0 && (module.exports = {
   AmountError,
   CurrencyMismatchError,
   CurrencyUnknownError,
@@ -1264,5 +1303,5 @@ export {
   hasCurrency,
   loadCurrencyMap,
   registerCurrency
-};
-//# sourceMappingURL=index.js.map
+});
+//# sourceMappingURL=index.cjs.map
